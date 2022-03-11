1f4a9a361ffebdfe4800a316c5646a1f08d7c5a1
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-03-11 15:06:53 +0100

    fix ant-matcher

--- backend/src/main/java/de/kittlaus/backend/controller/ReactController.java
+++ backend/src/main/java/de/kittlaus/backend/controller/ReactController.java
@@ -2,13 +2,13 @@ package de.kittlaus.backend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ReactController {
 
     @RequestMapping(value = "/**/{[path:[^\\.]*}")
     public String forwardToRouteUrl() {
         return "forward:/";
     }
-
 }
