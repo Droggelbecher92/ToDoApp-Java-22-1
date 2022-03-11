c7234cd1cf1c515fcb51b8d9c15511f894cecad0
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-03-11 14:53:15 +0100

    React fix

---
+++ backend/src/main/java/de/kittlaus/backend/controller/ReactController.java
@@ -0,0 +1,14 @@
+package de.kittlaus.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactController {
+
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String forwardToRouteUrl() {
+        return "forward:/";
+    }
+
+}
